8c8abba7e24d958e11dd69f8ae313e0ba5d970d3
milankarunarathne <mhkarunarathne@gmail.com>
2019-01-23 12:11:17 +0530

    Adding method Input Output counter

---
+++ relations_derivator.py
@@ -0,0 +1,15 @@
+import nltk as nltk
+
+class BooleanExpressionCreator:
+    'Generating Boolean expression from truth table'
+
+    def input_number_identifier(self):
+        print(1)
+
+    def metrixCreator(self):
+        print(2)
+
+
+
+class RelationsExtractor:
+    'Deriving Relationships between Inputs and Outputs'
